f41d7b596cbe8173a1607e416ae75f8da8f3f649
hrqmonteiro <hrqmonteiro@protonmail.com>
2023-05-29 19:10:56 -0300

    feat: added new fonts, updated lspconfig on nvim, gnome-shell themes and chezmoi script

--- dot_config/nvim/lua/plugins/lsp-config.lua
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -3,14 +3,14 @@ require("mason-lspconfig").setup()
 
 local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
--- require("lspconfig").ccls.setup {
---   capabilities = capabilities,
---   init_options = {
---     cache = {
---       directory = ".ccls-cache"
---     }
---   }
--- }
+require("lspconfig").ccls.setup {
+  capabilities = capabilities,
+  init_options = {
+    cache = {
+      directory = ".ccls-cache"
+    }
+  }
+}
 
 require("lspconfig").tsserver.setup {
   capabilities = capabilities
